aabffb5e8f687bde6ecb68cf6d8fc3bcbe4f1e54
Eugenio Angriman <eugenio.angriman@informatik.hu-berlin.de>
2018-10-23 16:52:08 +0200

    Clang format of DegreeCentrality class
    Signed-off-by: Eugenio Angriman <eugenio.angriman@informatik.hu-berlin.de>

--- networkit/cpp/centrality/DegreeCentrality.cpp
+++ networkit/cpp/centrality/DegreeCentrality.cpp
@@ -9,35 +9,29 @@
 
 namespace NetworKit {
 
-DegreeCentrality::DegreeCentrality(const Graph& G, bool normalized, bool outDeg, bool ignoreSelfLoops) : Centrality(G, normalized), outDeg(outDeg), ignoreSelfLoops(ignoreSelfLoops) {
-}
+DegreeCentrality::DegreeCentrality(const Graph &G, bool normalized, bool outDeg,
+                                   bool ignoreSelfLoops, const bool weighted)
+    : Centrality(G, normalized), outDeg(outDeg),
+      ignoreSelfLoops(ignoreSelfLoops), weighted(weighted) {}
 
 void DegreeCentrality::run() {
 	scoreData = std::vector<double>(G.upperNodeIdBound(), 0.0);
 
 	if (G.isDirected() && !outDeg) {
-		G.parallelForNodes([&](node u) {
-			scoreData[u] = G.degreeIn(u);
-		});
+		G.parallelForNodes([&](node u) { scoreData[u] = G.degreeIn(u); });
 	} else {
-		G.parallelForNodes([&](node u) {
-			scoreData[u] = G.degree(u);
-		});
+		G.parallelForNodes([&](node u) { scoreData[u] = G.degree(u); });
 	}
 
 	if (normalized) {
 		count maxDeg = maximum();
-		G.parallelForNodes([&](node u) {
-			scoreData[u] = scoreData[u] / maxDeg;
-		});
+		G.parallelForNodes([&](node u) { scoreData[u] = scoreData[u] / maxDeg; });
 	}
 	hasRun = true;
 }
 
-
 double DegreeCentrality::maximum() {
 	return G.numberOfNodes() - ignoreSelfLoops;
 }
-
 
 } /* namespace NetworKit */
