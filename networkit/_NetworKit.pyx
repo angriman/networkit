6af4d9534701ba4b1d621f0408169cc75ffa6fdf
Eugenio Angriman <eugenio.angriman@informatik.hu-berlin.de>
2020-01-24 16:51:55 +0100

    Fix typo in variable

--- networkit/_NetworKit.pyx
+++ networkit/_NetworKit.pyx
@@ -12031,7 +12031,7 @@ cdef extern from "<networkit/centrality/ApproxEffectiveResistance.hpp>" namespac
 		Random = 1
 		MinApproxEcc = 2
 
-class _ROotStrategy(object):
+class _RootStrategy(object):
 	maxdegree = MaxDegree
 	random = Random
 	minapproxecc = MinApproxEcc
